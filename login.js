970380f39e99cc9c391187b981cb54e6e8464d34
AZAD <azadgupta1010@gmail.com>
2025-05-17 18:01:43 +0530

    for app

--- login.js
+++ login.js
@@ -40,7 +40,13 @@
     }
   });
   
-    
+    if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('service-worker.js')
+        .then(reg => console.log('✅ Service Worker registered'))
+        .catch(err => console.log('❌ Service Worker failed', err));
+    });
+  }
 
 
 
